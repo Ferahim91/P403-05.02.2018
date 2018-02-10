c36ee415a0c99cd602660a23a058b2e6c842d016
Ferahim91 <34597804+Ferahim91@users.noreply.github.com>
2018-02-10 21:08:30 +0400

    Alqoritm folderin elave etdim

---
+++ p403-10-02-2018-Ferahim91/10.02.2018/Code/main.js
@@ -0,0 +1,81 @@
+// Iki reqemin ceminin tapilmasi
+
+// var firstNumber = Number(prompt("Ilk reqemi daxil edin!: "));
+// var secondNumber = Number(prompt("Ikinci reqemi daxil edin!: "));
+
+// var Sum = firstNumber + secondNumber;
+
+// alert("Yazdiginiz reqemlerin cemi: " + Sum);
+
+
+// Tek ve ya cut reqemin yoxlanilmasi
+
+// var a = Number(prompt("Reqem daxil edin: "));
+// if(a % 2 == 0){
+// 	console.log("Cutdur");
+// }else{
+// 	console.log("Tekdir");
+// }
+
+
+
+// 3 reqem arasinda maximum-un tapilmasi
+
+// var N1 = Number(prompt("Ilk reqemi daxil edin!: "));
+// var N2 = Number(prompt("Ikinci reqemi daxil edin!: "));
+// var N3 = Number(prompt("Ununcu reqemi daxil edin!: "));
+
+// if(N1 > N2){
+//     if(N1 > N3){
+//         console.log(N1 + " en boyukdur");
+//     }else{
+//         console.log(N3 + " en boyukdur");
+//     }
+// }else{
+//     if(N2 > N3){
+//         console.log(N2 + " en boyukdur");
+//     }else{
+//         console.log(N3 + " en boyukdur");
+//     }
+// }
+
+// 0
+// ""
+// undefined
+// null
+// NaN
+
+// var x = 0;
+
+// if(x){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+// Short circuiting
+// var x = 4 && 5;
+
+// console.log(x);
+
+// Other mathematical operators
+// var x = 5;
+// console.log(x);
+
+// x += 5 * 7;
+// console.log(x);
+
+// x *= 2;
+// console.log(x);
+
+// x /= 4;
+// console.log(x);
+
+// x %= 3;
+// console.log(x);
+
+// Number divisible by 3 and 7
+var N = Number(prompt("Reqem daxil edin: "));
+
+
+
---
+++ p403-10-02-2018-Ferahim91/README.md
@@ -0,0 +1,19 @@
+# p403-10-02-2018
+
+### Tapşırıq (Flowchart)
+
+1) Tələbənin imtahandan aldığı qiymət (1-100 arası) input olaraq verilir. Sizin alqoritm aşağıdakı cədvələ uyğun olaraq istifadəçiyə bir grade (dərəcə) qaytarmalıdır: Percentage >= 90 : Grade A Percentage >= 80 : Grade B Percentage >= 70 : Grade C Percentage >= 60 : Grade D Percentage >= 40 : Grade E Percentage < 40 : Grade F
+
+
+### Tapşırıq (Oxumaq üçün)
+
+#### Data tiplər haqqında:
+-	*https://www.tutorialspoint.com/csharp/csharp_data_types.htm*
+
+#### Operatorlar və onların növləri haqqında
+- *https://www.programiz.com/c-programming/c-operators*
+- *https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence*
+
+*Flowchart-ı şəkil kimi repository-ə yükləmək lazımdır. Deadline sabah səhər 9-dur.*
+
+*Sabaha qədər. Uğurlar.*
